bba6deb7a677e8546e3226b5ba7c47a15b385b24
Dami <obayanjud@gmail.com>
2019-02-10 11:24:38 -0800

    design

--- js/app.js
+++ js/app.js
@@ -157,3 +157,49 @@ document.querySelector("button#start").addEventListener("click", async () => {
   console.log("Using media constraints:", constraints);
   await init(constraints);
 });
+
+//  -------------- upload image --------------- //
+$(function() {
+  $(":file").change(function() {
+    if (this.files && this.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = imageIsLoaded;
+      reader.readAsDataURL(this.files[0]);
+    }
+  });
+});
+
+function imageIsLoaded(e) {
+  $("#myImg").attr("src", e.target.result);
+  $("#yourImage").attr("src", e.target.result);
+}
+function previewFile() {
+  var preview = document.querySelector("img.image-option"); //selects the query named img
+  var file = document.querySelector("input[type=file]").files[0]; //sames as here
+  var reader = new FileReader();
+
+  reader.onloadend = function() {
+    preview.src = reader.result;
+  };
+
+  if (file) {
+    reader.readAsDataURL(file); //reads the data as a URL
+  } else {
+    preview.src = "";
+  }
+}
+
+previewFile();
+
+//  -------------- upload video --------------- //
+var URL = window.URL || window.webkitURL;
+var video = document.querySelector("video#video-option");
+function onChange() {
+  var fileItem = document.getElementById("fileItem");
+  var files = fileItem.files;
+  var file = files[0];
+  var url = URL.createObjectURL(file);
+  video.src = url;
+  video.load();
+}
